05ce058776a531d7f481978fa9c75d1f8481472d
John Gizis <gizis@udel.edu>
2018-03-06 12:29:22 -0500

    move loop into k2ucd

---
+++ process_all.py
@@ -0,0 +1,17 @@
+import numpy as np
+import k2ucd
+#
+#  we could use glob to grab all the lists, but I prefer to control that by
+#  making this script
+k2ucd.periodloop('c3list.txt')
+k2ucd.periodloop('c4list.txt')
+k2ucd.periodloop('c5list.txt')
+k2ucd.periodloop('c6list.txt')
+k2ucd.periodloop('c7list.txt')
+k2ucd.periodloop('c8list.txt')
+k2ucd.periodloop('c10list.txt')
+k2ucd.periodloop('c12list.txt')
+k2ucd.periodloop('c13list.txt')
+k2ucd.periodloop('c14list.txt')
+#
+
