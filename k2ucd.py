2cc6569b218ffc60c213291046622eb0b9b80fbb
John Gizis <gizis@udel.edu>
2018-03-06 12:32:23 -0500

    fix k2ucd for commit

--- k2ucd.py
+++ k2ucd.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#read
+# uses astropy
 import astropy.io.ascii as ascii
 from astropy.table import Table
 from astropy.io import fits
@@ -70,3 +70,30 @@ def periodcheck(thistime,thisflux,mflags):
     #
     return frequency,power,best_frequency,best_fap
 #
+#  subroutine to loop through all sources of a given campaign.  
+def periodloop(cfile):
+    # EPIC numbers are 9 digits:
+    epic_length=9
+    # debugging only:
+    #print(cfile)
+    # how to read simple ascii file
+    #   https://oceanpython.org/2012/12/02/reading-text-ascii-files/
+    filelist = open(cfile, 'r') # 'r' = read
+    for myline in filelist:
+        # grab the EPIC name
+        k2name=myline.strip()
+        ispot_epic=k2name.find('EPIC')
+        epic_number=k2name[ispot_epic+5:ispot_epic+5+epic_length]
+        # read the data
+        dates,flux,flux_pcor,flux_ptcor,mflags = readpsfk2cor(k2name)
+        # compute best fitting frequency in Lomb-Scargle Periodogram
+        frequency,power,best_frequency,best_fap=periodcheck(dates,flux_pcor,mflags)
+        #
+        # now let's do it again, but with the position and time corrected version
+        freq_pt,power_pt,best_pt_frequency,best_pt_fap=periodcheck(dates,flux_ptcor,mflags)
+        #
+        #print(best_frequency)
+        print('{0:9.5f} {1:8.4f} {2:12.4e} {3:8.4f} {4:12.4e} {5}'.format(1.0/best_frequency,
+                24.0/best_frequency,best_fap,24.0/best_pt_frequency,best_pt_fap,epic_number))
+        #
+    filelist.close()
